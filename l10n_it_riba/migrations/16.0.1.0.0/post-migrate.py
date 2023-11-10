878049abfca5c3e6f0824bf8fde398051dc6a5ab
odooNextev <odoo@nextev.it>
2023-11-10 14:23:12 +0100

    [FIX] l10n_it_riba: add migration to remove distinta refs

--- l10n_it_riba/migrations/16.0.1.0.0/post-migrate.py
+++ l10n_it_riba/migrations/16.0.1.0.0/post-migrate.py
@@ -13,4 +13,19 @@ def migrate(env, version):
         NEW_MODULE_NAME,
         "migrations/16.0.1.0.0/data/noupdate.xml",
     )
-    # TODO selection keys of riba.distinta[.line].state have changed
+    # remove riba.distinta[.line].state refs
+    distinta_line_state_refs = env["ir.model.data"].search(
+        [
+            ("module", "=", "l10n_it_riba"),
+            ("name", "like", "selection__riba_distinta_line__state__%"),
+        ]
+    )
+    distinta_line_state_refs.unlink()
+    # remove riba.distinta.state refs
+    distinta_line_refs = env["ir.model.data"].search(
+        [
+            ("module", "=", "l10n_it_riba"),
+            ("name", "like", "selection__riba_distinta__state%%"),
+        ]
+    )
+    distinta_line_refs.unlink()
